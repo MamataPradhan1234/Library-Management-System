e80c1cb78e552f76a85b233849517158c4f3c307
mamata <pradhan.mamata678@gmail.com>
2023-10-21 00:46:17 +0530

    Add initial code to index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+require('./config/mongoose');
+require('./config/passport-jwt-strategy')
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error is running on server:${err}`);
+    }
+    console.log(`server is running on port:${port}`);
+})
