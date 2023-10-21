568eea2b2fa8c5531004d450cc66107d5f15c66e
mamata <pradhan.mamata678@gmail.com>
2023-10-21 21:43:53 +0530

    Add the config folder

---
+++ config/middleware.js
@@ -0,0 +1,19 @@
+// middleware.js
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join(__dirname, '..', 'uploads', 'users', 'avatars');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, AVATAR_PATH);
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const uploads = multer({ storage: storage });
+const uploadAvatar = uploads.single('avatar');
+
+module.exports = uploadAvatar;
