19703599017db6f1f54c5ec395e3794a665a9ffa
mamata <pradhan.mamata678@gmail.com>
2023-10-23 16:58:35 +0530

    Add the routes  folder

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const passport=require('passport');
-const uploadAvatar =require('../config/middleware');
+const middleware =require('../config/middleware');
+const uploadAvatar=middleware.uploadAvatar;
 
 const userController=require('../controllers/users_controller');
 
